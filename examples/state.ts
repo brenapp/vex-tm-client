79e05909c5768c2f7e557646672e1d2e83bce502
Brendan McGuire <brendan@bren.app>
2023-11-27 20:38:11 -0600

    State example

---
+++ examples/state.ts
@@ -0,0 +1,37 @@
+import { Client, FieldsetAudienceDisplay, FieldsetQueueSkillsType, MatchRound } from "vex-tm-client"
+import authorization from "./credentials.json"
+import { FieldsetEvent } from "../out/Fieldset";
+
+(async function () {
+
+    const client = new Client({
+        address: "http://localhost",
+        authorization: {
+            client_id: authorization.client_id,
+            client_secret: authorization.client_secret,
+            grant_type: "client_credentials",
+            expiration_date: authorization.expiration_date
+        }
+    });
+
+    const result = await client.connect();
+    if (!result.success) {
+        console.error("client", result);
+        return;
+    }
+
+    const fieldsets = await client.getFieldsets();
+    if (!fieldsets.success) {
+        console.error("fieldsets", fieldsets);
+        return;
+    }
+
+    const fieldset = fieldsets.data[0];
+
+    await fieldset.connect();
+
+    fieldset.on("message", (e: FieldsetEvent) => {
+        console.log(e, fieldset.state);
+    });
+
+})();
